45866673498d4ff2eed61e61c73879edffbca892
Daniel Hoffmann <daniel.hoffmann@student.hpi.uni-potsdam.de>
2014-10-25 14:00:43 +0200

    Clean up

--- pubmed_connector.py
+++ pubmed_connector.py
@@ -8,7 +8,6 @@ from datamodel.datamodel import Paper
 class PubMedConnector(APIConnector):
 
     def __init__(self):
-        #super(PubMedConnector, self).__init__()
         self.api_id = 'pubmed'
 
     def create_paper(self, paper_json):
@@ -28,16 +27,13 @@ class PubMedConnector(APIConnector):
 
     def search_doi(self, doi):
         url = "https://www.ebi.ac.uk/europepmc/webservices/rest/search/query=%s%%20sort_cited:y&format=json" % doi
-        #print url
         r = requests.get(url)
         result_json = json.loads(r.content)['resultList']['result']
         papers = []
         references = []
         if len(result_json) > 0:
             paper_json = result_json[0]
-            papers.append(self.create_paper(paper_json)) 
-            #print(json.dumps(paper_json, sort_keys=True, 
-            #    indent=4, separators=(',', ': ')))
+            papers.append(self.create_paper(paper_json))
 
             if paper_json['hasReferences'] == "Y":
                 collection = paper_json['source']
@@ -48,14 +44,9 @@ class PubMedConnector(APIConnector):
                 result_json = json.loads(r.content)['referenceList']['reference']
 
                 if len(result_json) > 0:
-                    #print len(result_json)
-                    #reference_json = result_json[0]
-                    #print(json.dumps(reference_json, sort_keys=True, 
-                    #    indent=4, separators=(',', ': ')))
 
                     for result in result_json:
                         if result.has_key('title'):
-                            #print result['title']
                             references.append(self.create_cited_paper(result))
 
             papers[0].references = references
