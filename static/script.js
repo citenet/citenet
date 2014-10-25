e2307d8b2582b8bb3b07c1becbfbe5ad48804fdd
optikfluffel <optik@fluffel.io>
2014-10-26 00:58:25 +0200

    more cleanup and some comments, time to ZZzzz now

--- static/script.js
+++ static/script.js
@@ -8,23 +8,38 @@ $( document ).ready(function() {
 
     var $title = $(".document-title");
 
+    // remove existing visualization
+    var $existingSvg = $("#vis").find("svg");
+    if ($existingSvg.length > 0) { $existingSvg.remove(); }
+
+    // create and add spinner
     var $spinner = $("<span />").addClass("glyphicon")
                                 .addClass("glyphicon-refresh")
                                 .addClass("glyphicon-refresh-animate");
+    $title.empty()
+          .text("Loading ")
+          .addClass("spinner")
+          .addClass("text-center")
+          .append($spinner);
 
-    var $wrappedSpinner = $("<h1 />").addClass("text-center")
-                                     .append($spinner);
-
-    $wrappedSpinner.insertAfter($title);
-
+    // make ajax post and handle received data
     $.post(action, function(data) {
-      $title.text(documentId);
+      // set new title
+      var title = data.object[documentId].title;
+      $title.empty()
+            .removeClass("spinner")
+            .removeClass("text-center")
+            .text(title);
+
+      // create and add subtitle to title
+      var $subtitle = $('<small />').text(" [" + documentId + "]")
+                                    .appendTo($title);
+
+      // remove spinner
       $title.next("h1").remove();
 
-      var $existingSvg = $("#vis").find("svg");
-      if ($existingSvg.length > 0) { $existingSvg.remove(); }
-
-      visualizeStuff(data);
+      // new visualization
+      visualizeStuff(data); // TODO: update existing instead of remove + add
     });
   });
 
@@ -44,46 +59,54 @@ $( document ).ready(function() {
       return date.getFullYear();
     }
 
-    var color = d3.scale.category20();
+    // get constrains
+    var minYear = d3.min(publications, function(item) { return getYearFromDateString(item.date); });
+    var maxYear = d3.max(publications, function(item) { return getYearFromDateString(item.date); });
 
+    var minGlobalCitationCount = d3.min(publications, function(item) { return item.global_citation_count; });
+    var maxGlobalCitationCount = d3.max(publications, function(item) { return item.global_citation_count; });
+
+    // initialize force directed d3 layout
     var force = d3.layout.force()
                   .charge(-100)
                   .linkDistance(20)
                   .size([width, height]);
 
+    // add nodes to layout
+    // TODO: Figure out links and add them to force
+    force.nodes(publications)
+         .start();
+
+    // add svg to dom
     var svg = d3.select("#vis")
                 .append("svg")
                 .attr("width", width)
                 .attr("height", height);
 
-    var earliestYear = d3.min(publications, function(item) { return getYearFromDateString(item.date); });
-    var latestYear = d3.max(publications, function(item) { return getYearFromDateString(item.date); });
+    // helper to scale radius
+    var scaleRadius = d3.scale.sqrt()
+                              .domain([minGlobalCitationCount, maxGlobalCitationCount])
+                              .range([minRadius, maxRadius]);
 
+    // color helper
     var color = d3.scale.category20();
 
-    var minGlobalCitationCount = d3.min(publications, function(item) { return item.global_citation_count; });
-    var maxGlobalCitationCount = d3.max(publications, function(item) { return item.global_citation_count; });
-
-    var scale = d3.scale.sqrt()
-                        .domain([minGlobalCitationCount, maxGlobalCitationCount])
-                        .range([minRadius, maxRadius]);
-
-    // TODO: Figure out links and add them to force
-    force.nodes(publications)
-         .start();
-
+    // add nodes to svg
     var node = svg.selectAll(".node")
                   .data(publications)
                   .enter().append("circle")
                   .attr("class", "node")
-                  .attr("r", function(d) { return scale(d.global_citation_count); })
+                  .attr("r", function(d) { return scaleRadius(d.global_citation_count); })
                   .style("fill", function(d) { return color(getYearFromDateString(d.date)); })
 
+    // add title to nodes
     node.append("title")
         .text(function(d) { return d.title + " | Citations: " + d.global_citation_count + " | date: " + d.date; });
 
-    var timeScale = d3.time.scale().domain([latestYear, earliestYear]).range([0, height]);
+    // scale foo
+    var timeScale = d3.time.scale().domain([maxYear, minYear]).range([0, height]);
 
+    // handle positioning of circles on svg
     force.on("tick", function() {
       node.attr("cx", function(d) { return d.x = stayInsideBoxWidth(d.x); })
           .attr("cy", function(d) {
