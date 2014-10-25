42b50f3194fa0d5a8ac0d46a3f70695d66648c33
Joren Retel <jorenretel@hotmail.com>
2014-10-25 12:06:07 +0200

    Added datamodel

---
+++ webapp.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    dic = {"hello": "world"}
+    return jsonify(**dic)
+
+if __name__ == "__main__":
+    app.run(debug = True)
