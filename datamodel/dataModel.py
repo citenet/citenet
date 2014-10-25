2b97da1e059b163a51046cfa3ba75c094bbff4c8
Joren Retel <jorenretel@hotmail.com>
2014-10-25 12:11:09 +0200

    add doi and citation count field to datamodel

--- datamodel/dataModel.py
+++ datamodel/dataModel.py
@@ -2,16 +2,19 @@ from datetime import date
 
 class Paper(object):
 
-    def __init__(self, title=None, authors=None, date=None, references=None,
-                 api=None, api_id=None, openAccess=False):
+    def __init__(self, title=None, authors=None, date=None, doi=None,
+                 references=None, api=None, api_id=None, openAccess=False,
+                 incoming_citation_count=1):
 
         self.title = title
         self.authors = authors or set()
         self.date = date
+        self.doi = doi
         self.references = references or set()
         self.api = api
         self.api_id = api_id
         self.openAccess = openAccess
+        self.incoming_citation_count = incoming_citation_count
 
 
 
