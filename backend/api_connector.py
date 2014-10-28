a9b72e95033c94e086eced49506c97f65f27092e
Daniel Hoffmann <daniel.hoffmann@student.hpi.uni-potsdam.de>
2014-10-28 21:15:33 +0100

    Add caching for synchronous API calls using redis-simple-cache

--- backend/api_connector.py
+++ backend/api_connector.py
@@ -1,4 +1,13 @@
 import requests
+from redis_cache import cache_it
+
+@cache_it(limit=1000, expire=60 * 60 * 24 * 30)
+def make_call(url, head):
+    print "make_call", url
+    if head:
+        return requests.head(url)
+    else:
+        return requests.get(url) 
 
 class APIConnector(object):
 
@@ -8,13 +17,12 @@ class APIConnector(object):
 
     def call(self, url, head=False):
         url = self.base_url + url
-        if head:
-            return requests.head(url)
-        else:
-            return requests.get(url)
+        print "call", url
+        return make_call(url, head)
 
     def call_async(self, url, session, callback):
         url = self.base_url + url
+        print "call_async", url
         session.get(url, background_callback=callback)
 
     def search_doi(self, doi, get_references=False):
